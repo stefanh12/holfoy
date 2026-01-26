7762caa790835bf21d59447bf1bfd96db008ec36
Stefan <wan73605@burktom.anonaddy.com>
2026-01-26 12:48:25 +0100

    lint

--- custom_components/holfuy/__init__.py
+++ custom_components/holfuy/__init__.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 import aiohttp
 import async_timeout
 
